523fe36a310ead94ae23b5ed2b5d8dce8f182cd4
Rey04rdmf <105935945+Rey04rdmf@users.noreply.github.com>
2025-09-27 16:39:16 -0300

    Update notebook1.py

--- Proyecto_1/notebook1.py
+++ Proyecto_1/notebook1.py
@@ -1,6 +1,5 @@
 ### 📎 Enlace al notebook
 
-[🔗 Ver notebook en Google Colab]
-https://colab.research.google.com/drive/1zegFmpEMQfC4oL7VANeUwXfV9v0akf34?usp=sharing
+🔗 [Ver notebook en Google Colab](https://colab.research.google.com/drive/1zegFmpEMQfC4oL7VANeUwXfV9v0akf34?usp=sharing)
 
 Este notebook contiene el análisis exploratorio del dataset de presupuestos y programas gubernamentales en NYC. Incluye limpieza de datos, visualizaciones por categoría y observaciones clave sobre la distribución de fondos.
