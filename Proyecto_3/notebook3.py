b13b559c502c9b0c0c11d603982effba4178dbdc
Rey04rdmf <105935945+Rey04rdmf@users.noreply.github.com>
2025-09-28 05:44:03 -0300

    Create notebook3.py

---
+++ Proyecto_3/notebook3.py
@@ -0,0 +1,9 @@
+# 📎 Enlace al notebook
+
+# 🔗 Ver notebook en Google Colab:
+# https://colab.research.google.com/drive/1tjmxwd46UJ1BNLND-z2FFHogotg7yToZ?usp=sharing
+
+# Este notebook contiene el análisis exploratorio del dataset de rendimiento en construcción.
+# Incluye revisión de precios de materiales, mano de obra, indicadores económicos y demográficos.
+# Se aplicaron visualizaciones como histogramas, gráficos de dispersión y mapas de calor
+# para detectar correlaciones entre variables y patrones regionales.
