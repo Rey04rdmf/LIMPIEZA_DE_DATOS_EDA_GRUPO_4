65f53a479b120b86ffbd62b2aa199589d1417c29
Rey04rdmf <105935945+Rey04rdmf@users.noreply.github.com>
2025-09-28 05:34:59 -0300

    Create notebook2.py

---
+++ Proyecto_2/notebook2.py
@@ -0,0 +1,9 @@
+# 📎 Enlace al notebook
+
+# 🔗 Ver notebook en Google Colab:
+# https://colab.research.google.com/drive/1N6PwGyFeNH6-2phzEt2wRQ_3WI89HhMG?usp=sharing
+
+# Este notebook contiene el análisis exploratorio del dataset de propiedades en venta en España.
+# Incluye limpieza profunda de variables, normalización de precios, visualizaciones geográficas,
+# histogramas, gráficos de dispersión y observaciones clave sobre la relación entre ubicación,
+# materiales y precios.
