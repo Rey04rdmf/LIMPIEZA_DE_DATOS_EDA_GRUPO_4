ea8d9d2542e8b72b37b2586b8da4f82a2e5f11eb
Rey04rdmf <105935945+Rey04rdmf@users.noreply.github.com>
2025-09-28 05:51:14 -0300

    Create notebook4.py

---
+++ Proyecto_4/dataset/notebook4.py
@@ -0,0 +1,9 @@
+# 📎 Enlace al notebook
+
+# 🔗 Ver notebook en Google Colab:
+# https://colab.research.google.com/drive/1rFjzJpvZ7fBrdepFBszXswD2P8J0U4nV?usp=sharing
+
+# Este notebook contiene el análisis exploratorio del dataset de permisos de obra en la Ciudad Autónoma de Buenos Aires.
+# Incluye limpieza de datos, conversión de fechas, visualizaciones por barrio, comuna y tipo de permiso.
+# Se analizaron tendencias temporales, distribución geográfica y frecuencia de tipos de obra.
+# El análisis reveló patrones claros en barrios como Palermo, Belgrano y Caballito, así como un predominio de permisos de obra nueva.
